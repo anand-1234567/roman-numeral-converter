2424a1b2e949ebd7d05a0e8bd1b766c5ca08de5d
Anand Gorantala <ianand2@gmail.com>
2025-02-22 12:57:32 -0800

    chore: add test coverage script

--- apps/api/src/instrumentation.ts
+++ apps/api/src/instrumentation.ts
@@ -2,6 +2,8 @@ import { registerOTel } from '@vercel/otel';
  
 export async function register() {
   registerOTel({ serviceName: process.env.APP_NAME || 'roman-numeral-converter-api' });
+  // Only load the instrumentation for the nodejs runtime
+  // to prevent throwing an error that it is not supported for the edge runtime
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./instrumentation-node')
   }
